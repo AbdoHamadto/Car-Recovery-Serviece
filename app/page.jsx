2afde3af5fa5db7fedb1344ce9a4cc0dcd015109
Ubada <bhassan.ghamsh2020@gmail.com>
2024-11-29 23:05:08 +0200

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,14 @@
 import config from "@config/config.json";
-import Cta from "@layouts/components/Cta";
 import SeoMeta from "@layouts/SeoMeta";
 import home from "@config/home.json"
 
 import HomeBanner from "@layouts/partials/HomeBanner";
 import FeaturesSection from "@layouts/partials/FeaturesSection";
-import Workflow from "@layouts/partials/Workflow";
-import { getListPage } from "../lib/contentParser";
 import Section from "@layouts/partials/Section";
 import TowingServiceSteps from "@layouts/partials/TowingServiceSteps";
 import MySwiperComponent from "@layouts/partials/SwiperComponent";
 
 const Home = async () => {
-  const homePage = await getListPage("content/_index.md");
-  const { frontmatter } = homePage;
-  const { banner, feature, services, workflow, call_to_action } = frontmatter;
   const { title } = config.site;
 
   return (
